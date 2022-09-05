d9582aa3541523585b59cbaf61f408fe4128deec
Казакова Ульяна Викторовна <kyv@krs.rcfh.local>
2022-09-05 16:54:45 +0700

    problems with store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+//import {createApp} from 'vue'
 import App from './App.vue'
+import store from './vuex/store'
 import './assets/styles/styles.scss'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+//createApp(App).mount('#app')
+/*const app = createApp({
+    App
+})
+app.use(store)
+app.mount('#app')*/
+new Vue({
+    render: h => h(App),
+    store
+}).$mount('#app')
+
+//createApp({App, store}).mount('#app')
